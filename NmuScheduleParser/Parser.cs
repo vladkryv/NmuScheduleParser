1db733e6bd4acc39a1d7dec39fc2ba426143d3ed
vladkryv <vladislavkrivovyaz@gmail.com>
2021-10-30 21:11:02 +0300

    first commit

---
+++ NmuScheduleParser/Parser.cs
@@ -0,0 +1,22 @@
+﻿using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NmuScheduleParser
+{
+    public class Parser
+    {
+        public async Task<Schedule> GetSchedule(string rawHtmlCode)
+        {
+            var context = BrowsingContext.New(Configuration.Default.WithDefaultLoader());
+            var document = await context.OpenAsync(r => r.Content(rawHtmlCode));
+
+            var cellSelector = "div.container div.row div.col-md-6:not(.col-xs-12)";
+            var cells = document.QuerySelectorAll(cellSelector);
+            
+            return null;
+        }
+    }
+}
